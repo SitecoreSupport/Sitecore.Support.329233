e80e39d989c7b017f3b3e5d79f0ee791f2005f25
DK\tpe <tpe@sitecore.net>
2019-05-28 16:58:38 +0300

    Handle 404 error in the HandleServerErrorCode

--- src/Sitecore.Support.329233/XA/Feature/ErrorHandling/Pipelines/HtpRequestProcessed/HandleServerErrorCode.cs
+++ src/Sitecore.Support.329233/XA/Feature/ErrorHandling/Pipelines/HtpRequestProcessed/HandleServerErrorCode.cs
@@ -2,56 +2,79 @@
 using System.Linq;
 using System.Web;
 using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Configuration;
 using Sitecore.DependencyInjection;
 using Sitecore.Diagnostics;
 using Sitecore.IO;
+using Sitecore.Links;
 using Sitecore.Pipelines.HttpRequest;
 using Sitecore.Web;
 using Sitecore.XA.Foundation.Abstractions;
 using Sitecore.XA.Foundation.Multisite;
 
-namespace Sitecore.XA.Feature.ErrorHandling.Pipelines.HtpRequestProcessed
+namespace Sitecore.Support.XA.Feature.ErrorHandling.Pipelines.HtpRequestProcessed
 {
-    public class HandleServerErrorCode : HttpRequestProcessor
+  public class HandleServerErrorCode : HttpRequestProcessor
+  {
+    protected ISiteInfoResolver SiteInfoResolver;
+    protected IContext Context { get; } = ServiceLocator.ServiceProvider.GetService<IContext>();
+
+    public HandleServerErrorCode()
     {
-        protected ISiteInfoResolver SiteInfoResolver;
-        protected IContext Context { get; }= ServiceLocator.ServiceProvider.GetService<IContext>();
-
-        public HandleServerErrorCode()
-        {
-            SiteInfoResolver = ServiceLocator.ServiceProvider.GetService<ISiteInfoResolver>();
-        }
-
-        public override void Process(HttpRequestArgs args)
-        {
-            if (args.Context.Error != null && !Context.Site.Name.Equals("shell"))
-            {
-                var siteInfos = GetPossibleSites();
-                var site = SiteInfoResolver.ResolveSiteFromRequest(siteInfos, new HttpRequestWrapper(args.Context.Request));
-
-                if (site != null)
-                {
-                    var url = GetStaticErrorPageUrl(site);
-                    if (File.Exists(FileUtil.MapPath(url)))
-                    {
-                        args.Context.Server.TransferRequest(url);
-                    }
-                    else
-                    {
-                        Log.Warn($"Could not find proper static error page for site: {site.Name}. Please generate it.", this);
-                    }
-                }
-            }
-        }
-
-        protected virtual SiteInfo[] GetPossibleSites()
-        {
-            return SiteInfoResolver.Sites.OrderByDescending(info => info.VirtualFolder.Length).ToArray();
-        }
-
-        protected virtual string GetStaticErrorPageUrl(SiteInfo site)
-        {
-            return $"{Constants.ErrorPagesFolder}/{site.Name}.html";
-        }
+      SiteInfoResolver = ServiceLocator.ServiceProvider.GetService<ISiteInfoResolver>();
     }
+
+    public override void Process(HttpRequestArgs args)
+    {
+      if (args.Context.Error != null && !Context.Site.Name.Equals("shell"))
+      {
+
+        if (args.Context.Error is HttpException error)
+          if (error.GetHttpCode() == 404)
+          {
+            var settingsItem = ServiceLocator.ServiceProvider.GetService<IMultisiteContext>().GetSettingsItem(Context.Database.GetItem(Context.Site.StartPath));
+            if (settingsItem != null)
+            {
+              var errorPageItem =
+                LinkManager.GetItemUrl(Context.Database.GetItem(settingsItem[Sitecore.XA.Feature.ErrorHandling.Templates._ErrorHandling.Fields.Error404Page] ?? ""));
+
+              if (Settings.RequestErrors.UseServerSideRedirect)
+                HttpContext.Current.Server.TransferRequest(errorPageItem);
+              else
+                HttpContext.Current.Response.Redirect(errorPageItem);
+
+              args.AbortPipeline();
+            }
+          }
+
+        var siteInfos = GetPossibleSites();
+        var site = SiteInfoResolver.ResolveSiteFromRequest(siteInfos, new HttpRequestWrapper(args.Context.Request));
+
+        if (site != null)
+        {
+          var url = GetStaticErrorPageUrl(site);
+          if (File.Exists(FileUtil.MapPath(url)))
+          {
+            args.Context.Server.TransferRequest(url);
+          }
+          else
+          {
+            Log.Warn($"Could not find proper static error page for site: {site.Name}. Please generate it.", this);
+          }
+        }
+      }
+    }
+
+    protected virtual SiteInfo[] GetPossibleSites()
+    {
+      return SiteInfoResolver.Sites.OrderByDescending(info => info.VirtualFolder.Length).ToArray();
+    }
+
+    protected virtual string GetStaticErrorPageUrl(SiteInfo site)
+    {
+      return $"{ErrorPagesFolder}/{site.Name}.html";
+    }
+
+    private const string ErrorPagesFolder = "/ErrorPages";
+  }
 }
